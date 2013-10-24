638ac4c0baacd43abd5aad376a7e5b55c7ff4c7c
Eric LaForce <elaforc@gmail.com>
2013-10-23 20:16:07 -0400

    Cleaned up mediator indention

--- patterns/mediator/mediator.java
+++ patterns/mediator/mediator.java
@@ -2,99 +2,89 @@
 import java.util.ArrayList;
 
 //Mediator
-interface Mediator
-{
-  void send(String message, Colleague colleague);	
+interface Mediator {
+    void send(String message, Colleague colleague);
 }
 
 //Colleage interface 
-abstract class Colleague
-{
-	private Mediator mediator;
-  Colleague() {}
-	public Colleague(Mediator m) {
-    mediator = m;
-	}
+abstract class Colleague {
+    private Mediator mediator;
+    Colleague() {}
+    public Colleague(Mediator m) {
+        mediator = m;
+    }
 
-	//send a message via the mediator
-  void send(String message) {
-    mediator.send(message, this);
-	}
-	
-	//get access to the mediator
-	Mediator getMediator() {
-		return mediator;
-	}
+    //send a message via the mediator
+    void send(String message) {
+        mediator.send(message, this);
+    }
 
-  abstract void receive(String message);	
+    //get access to the mediator
+    Mediator getMediator() {
+        return mediator;
+    }
+
+    abstract void receive(String message);
 }
 
 // Concrete Mediator
-class ApplicationMediator implements Mediator
-{
-	private ArrayList<Colleague> colleagues;
+class ApplicationMediator implements Mediator {
+    private ArrayList < Colleague > colleagues;
 
-  ApplicationMediator() { 
-    colleagues = new ArrayList<Colleague>();
-  } 
+    ApplicationMediator() {
+        colleagues = new ArrayList < Colleague > ();
+    }
 
-  void addColleague(Colleague colleague) { 
-    colleagues.add(colleague);
-  }
+    void addColleague(Colleague colleague) {
+        colleagues.add(colleague);
+    }
 
-  // Note this is a very simple implementation that would not
-  // give you any additional benefit over Observer but 
-  // you could have some very complex notification logic here
-	public void send(String message, Colleague originator)	{
-		//let all other screens know that this screen has changed
-		for(Colleague colleague: colleagues)
-		{
-			//don't tell ourselves
-			if(colleague != originator)
-				colleague.receive(message);
-		}
-	}
+    // Note this is a very simple implementation that would not
+    // give you any additional benefit over Observer but 
+    // you could have some very complex notification logic here
+    public void send(String message, Colleague originator) {
+        //let all other screens know that this screen has changed
+        for (Colleague colleague: colleagues) {
+            //don't tell ourselves
+            if (colleague != originator)
+                colleague.receive(message);
+        }
+    }
 }
 
 // Concrete Colleague
-class ConcreteColleague extends Colleague
-{
-  public ConcreteColleague(ApplicationMediator applicationMediator) {
-    super(applicationMediator);
-  }
+class ConcreteColleague extends Colleague {
+    public ConcreteColleague(ApplicationMediator applicationMediator) {
+        super(applicationMediator);
+    }
 
-  void receive(String message)
-  {
-    System.out.println("Colleague Received: " + message);
-  }	
+    void receive(String message) {
+        System.out.println("Colleague Received: " + message);
+    }
 }
 
 // Concrete Colleague
-class MobileColleague extends Colleague
-{
-  public MobileColleague(ApplicationMediator applicationMediator) {
-    super(applicationMediator);
-  }
+class MobileColleague extends Colleague {
+    public MobileColleague(ApplicationMediator applicationMediator) {
+        super(applicationMediator);
+    }
 
-  void receive(String message)
-  {
-    System.out.println("Mobile Received: " + message);
-  }	
+    void receive(String message) {
+        System.out.println("Mobile Received: " + message);
+    }
 }
 
-class Client 
-{
-  public static void main(String[] args) 
-  {
-    ApplicationMediator mediator = new ApplicationMediator(); 
-      
-    ConcreteColleague desktop = new ConcreteColleague(mediator);
-    MobileColleague mobile = new MobileColleague(mediator);
-     
-    mediator.addColleague(desktop);
-    mediator.addColleague(mobile);
+class Client {
+    public static void main(String[] args) {
+        ApplicationMediator mediator = new ApplicationMediator();
 
-    desktop.send("Hello World");
-    mobile.send("Hello"); 
-  }
+        ConcreteColleague desktop = new ConcreteColleague(mediator);
+        MobileColleague mobile = new MobileColleague(mediator);
+
+        mediator.addColleague(desktop);
+        mediator.addColleague(mobile);
+
+        desktop.send("Hello World");
+        mobile.send("Hello");
+    }
 }
